8a51da14b9b7d65e2e9783847062760d3fb0053c
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-12-10 17:32:35 +0300

    Added Class creating example

---
+++ examples/class-creating/ClassCreating.java
@@ -0,0 +1,11 @@
+public class ClassCreating {
+	public static void main(String ... args){
+		String s = new String();
+		ClassCreating cc = new ClassCreating();
+		cc.sayHello();
+	}
+
+	public void sayHello(){
+		System.out.println("Hello, User!");
+	}
+}
